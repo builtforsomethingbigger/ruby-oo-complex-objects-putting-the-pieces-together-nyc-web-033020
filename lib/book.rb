8e03ef519840ee16b9e51ce5208c138492287552
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-03-31 23:29:06 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
+class Book
 
+    def initialize(title)
+        @title = title
+    end
+    attr_reader :title
+
+    attr_accessor :author, :page_count, :genre
+
+    # def author=(author)
+    #     @author = author
+    # end
+    # attr_accessor :author
+
+    # def page_count=(num)
+    #     @page_count = num
+    # end
+    # attr_accessor :page_count
+
+    # def genre=(genre)
+    #     @genre = genre
+    # end
+    # attr_accessor :genre
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
